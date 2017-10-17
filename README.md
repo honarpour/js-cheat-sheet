443290aa6e77b4222b1dc806cd3505930ac786c4
Amir Honarpour <honarpour@gmail.com>
2017-10-16 22:17:18 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## JavaScript Cheat Sheet
+
+`js-cheat-sheet`
+
+---
+
+### About
+
+Some handy JS code to make things a bit easier and prevent repetition.
+
+---
+
+### Get Started
+
+See `scripts.js`.
---
+++ scripts.js
@@ -0,0 +1,42 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+// "Document Ready"
+(function() {
+  console.log('Ready');
+})();
+
+function addEvent(selector, event, callback) {
+  $$(selector).forEach(function(element) {
+    element.addEventListener(event, callback);
+  });
+}
+
+function addClass(selector, className) {
+  $$(selector).forEach(function(element) {
+    element.classList.add(className);
+  });
+}
+
+function removeClass(selector, className) {
+  $$(selector).forEach(function(element) {
+    element.classList.remove(className);
+  });
+}
+
+function toggleClass(selector, className) {
+  $$(selector).forEach(function(element) {
+    element.classList.toggle(className);
+  });
+}
+
+function hasClass(selector, className) {
+  return Array.prototype.indexOf.call($(selector).classList, className) > -1;
+}
+
+function toggle(selector) {
+  const el = $(selector);
+  return '' === el.style.display
+    ? (el.style.display = 'none')
+    : (el.style.display = '');
+}
